f0a70d955e428358bc4badaee8b70be15368be41
Kalyan Chatterjee <kal.chatterjee@gmail.com>
2021-05-06 00:24:42 -0400

    personal: May 5th

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ElementsModule } from './elements/elements.module';
 import { CollectionsModule } from './collections/collections.module';
+import { HomeComponent } from './home/home.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, ElementsModule, CollectionsModule],
+  declarations: [AppComponent, HomeComponent, NotFoundComponent],
+  // https://www.udemy.com/course/the-modern-angular-bootcamp/learn/lecture/17674180#questions (order of the imports matter when it comes to routing.)
+  imports: [BrowserModule, ElementsModule, CollectionsModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
 })
