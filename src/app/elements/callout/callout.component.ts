1d385ba938d5016c739c45667ede0bb1193d8917
Kalyan Chatterjee <kal.chatterjee@gmail.com>
2021-05-11 23:15:31 -0400

    Personal: May 11

--- src/app/elements/callout/callout.component.ts
+++ src/app/elements/callout/callout.component.ts
@@ -1,15 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-callout',
   templateUrl: './callout.component.html',
-  styleUrls: ['./callout.component.css']
+  styleUrls: ['./callout.component.css'],
 })
 export class CalloutComponent implements OnInit {
+  @Input() button_url = '';
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
