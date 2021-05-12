f3ec39e10e30a8570add32ffd79bbc86caf57fc2
Kalyan Chatterjee <Kalyan.Chatterjee@cantire.com>
2021-05-12 16:46:14 -0400

    work: 12 May

--- src/app/collections/collections-home/collections-home.component.ts
+++ src/app/collections/collections-home/collections-home.component.ts
@@ -6,6 +6,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./collections-home.component.css'],
 })
 export class CollectionsHomeComponent implements OnInit {
+  data = [
+    { name: 'James', age: 24, job: 'Designer', started: '2020-12-14' },
+    { name: 'Jill', age: 26, job: 'Engineer', started: '2015-10-16' },
+  ];
+
+  headers = [
+    { key: 'name', label: 'Name' },
+    { key: 'age', label: 'Age' },
+    { key: 'job', label: 'Job' },
+    { key: 'started', label: 'Started' },
+  ];
   constructor() {}
 
   ngOnInit(): void {}
--- src/app/collections/collections.module.ts
+++ src/app/collections/collections.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { SharedModule } from '../shared/shared.module';
 import { CollectionsRoutingModule } from './collections-routing.module';
 import { CollectionsHomeComponent } from './collections-home/collections-home.component';
+import { TableComponent } from './table/table.component';
 
 @NgModule({
-  declarations: [CollectionsHomeComponent],
-  imports: [CommonModule, CollectionsRoutingModule],
+  declarations: [CollectionsHomeComponent, TableComponent],
+  imports: [CommonModule, CollectionsRoutingModule, SharedModule],
   // For a component inside this module to be accessible to other modules
   // it needs to be exported first.
   // In this case, we are using routing. Check collections-routing.module.ts
