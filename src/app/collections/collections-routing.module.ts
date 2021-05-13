d3145012858ad4f9bd3767b694029ea24ae8ae79
Kalyan Chatterjee <kal.chatterjee@gmail.com>
2021-05-13 14:59:58 -0400

    personal: 12 May

--- src/app/collections/collections-routing.module.ts
+++ src/app/collections/collections-routing.module.ts
@@ -2,11 +2,28 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 // Step 1 (for Routing) - import the components which gets routed to
 import { CollectionsHomeComponent } from './collections-home/collections-home.component';
+import { BiographyComponent } from './biography/biography.component';
+import { CompaniesComponent } from './companies/companies.component';
+import { PartnersComponent } from './partners/partners.component';
 
 const routes: Routes = [
   {
     path: '',
     component: CollectionsHomeComponent,
+    children: [
+      {
+        path: '',
+        component: BiographyComponent,
+      },
+      {
+        path: 'companies',
+        component: CompaniesComponent,
+      },
+      {
+        path: 'partners',
+        component: PartnersComponent,
+      },
+    ],
   },
 ];
 
