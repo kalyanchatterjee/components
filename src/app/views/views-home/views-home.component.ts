befc044b0df884f4e8fbd95e3897c8b6d1b8dfc3
Kalyan Chatterjee <Kalyan.Chatterjee@cantire.com>
2021-05-15 00:02:56 -0400

    work: May 14

---
+++ src/app/views/views-home/views-home.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-views-home',
+  templateUrl: './views-home.component.html',
+  styleUrls: ['./views-home.component.css'],
+})
+export class ViewsHomeComponent implements OnInit {
+  data: any[] = [
+    { value: 22, label: 'Faves' },
+    { value: 31200, label: 'Views' },
+    { value: 22, label: 'Members' },
+  ];
+
+  constructor() {}
+
+  ngOnInit(): void {}
+}
